e87d0620178d25b5ab6c1c55ea74bc7edeee0345
Min Kyung CHOI <haleychoi0918@gmail.com>
2020-12-03 07:36:37 +1100

    add redux products

--- src/components/Products.js
+++ src/components/Products.js
@@ -3,14 +3,22 @@ import formatCurrency from '../util'
 import Fade from 'react-reveal/Fade'
 import Modal from 'react-modal'
 import Zoom from 'react-reveal/Zoom'
+import { connect } from 'react-redux'
+import { fetchProducts } from '../actions/productActions'
 
-export default class Products extends Component {
+
+class Products extends Component {
     constructor(props){
         super(props)
         this.state = {
             product: null
         }
     }
+
+    componentDidMount(){
+        this.props.fetchProducts()
+    }
+
     openModal = (product) => {
         this.setState({product})
     }
@@ -22,7 +30,9 @@ export default class Products extends Component {
         return (
             <div>
                 <Fade bottom cascade={true}>
-                    <ul className="products">
+                    {
+                        !this.props.products ? <div>Loading...</div>:
+                        <ul className="products">
                         {this.props.products.map(product => (
                             <li key={product._id}>
                                 <div className="product">
@@ -44,6 +54,7 @@ export default class Products extends Component {
                             </li>
                         ))}
                     </ul>
+                    }
                 </Fade>
                 {
                     product && (
@@ -83,3 +94,6 @@ export default class Products extends Component {
         )
     }
 }
+
+
+export default connect((state) => ({products: state.products.items}),{fetchProducts})(Products)
